1e19d8824d438b2f1b3cda05fb11f49eaf8b0184
jinike <ji4926129@gmail.com>
2024-10-06 22:13:34 +0800

    短视频

--- App/HttpController/Api/Video/ShortVideoDy.php
+++ App/HttpController/Api/Video/ShortVideoDy.php
@@ -142,18 +142,18 @@ class ShortVideoDy extends UserBase
                     $vodIdArray[]=$v->vodId;
                 }
                 //是否已收藏
-                $collectRes=ShortVideoDyCollectRecordModel::create()->where(["uid"=>$userId])->where(["vod_id",$vodIdArray,"in"])->field("vod_id")->get();
-                return $this->writeJson(Status::CODE_OK, $collectRes, Status::getReasonPhrase(Status::CODE_OK));
-                foreach($data["list"] as $kl=>$vl){
-                    foreach($collectRes as $kc=>$vc){
-                            if($vl->vodId==$vc->vodId){
-                                $data["list"][$kl]->isCollect=1;   
-                            }
-                    }
-                }
+                // $collectRes=ShortVideoDyCollectRecordModel::create()->where(["uid"=>$userId])->where(["vod_id",$vodIdArray,"in"])->field("vod_id")->get();
+                
+                // foreach($data["list"] as $kl=>$vl){
+                //     foreach($collectRes as $kc=>$vc){
+                //             if($vl->vodId==$vc->vodId){
+                //                 $data["list"][$kl]->isCollect=1;   
+                //             }
+                //     }
+                // }
                 //是否已点击-心过
                 $clickRes=ShortVideoDyClickRecordModel::create()->where(["uid"=>$userId])->where(["vod_id",$vodIdArray,"in"])->get()->lastQuery();
-               
+                return $this->writeJson(Status::CODE_OK, $clickRes, Status::getReasonPhrase(Status::CODE_OK));
                 foreach($data["list"] as $kl=>$vl){
                     foreach($clickRes as $kc=>$vc){
                             if($vl->vodId==$vc->vodId){
