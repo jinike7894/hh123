75afb66122c4ee81f8700822438058c177b11ba2
jinike <ji4926129@gmail.com>
2024-10-06 21:30:29 +0800

    短视频

--- App/Model/Video/ShortVideoDyReplyModel.php
+++ App/Model/Video/ShortVideoDyReplyModel.php
@@ -23,10 +23,6 @@ class ShortVideoDyReplyModel extends BaseModel implements CommonStatusInterface
 {
     protected $tableName = 'mac_short_vod_dy_reply';
     protected $primaryKey = 'id';
-    protected $autoTimeStamp = 'datetime';
-    protected $createTime = 'createTime';
-    protected $updateTime = 'updateTime';
-
     const FILE_TYPE_UP = 'up';
     const FILE_TYPE_URL = 'url';
     const FILE_TYPE_AWS_S3 = 'awsS3';
