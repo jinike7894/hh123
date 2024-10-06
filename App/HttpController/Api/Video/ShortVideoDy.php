455623a04eda657007be19477055114defbbfd2e
jinike <ji4926129@gmail.com>
2024-10-06 22:34:37 +0800

    短视频

--- App/HttpController/Api/Video/ShortVideoDy.php
+++ App/HttpController/Api/Video/ShortVideoDy.php
@@ -156,9 +156,9 @@ class ShortVideoDy extends UserBase
               
                 foreach($data["list"] as $kl=>$vl){
                     foreach($clickRes as $kc=>$vc){
-                        
+                        $this->writeJson(Status::CODE_OK, $vl->vodId."data------", Status::getReasonPhrase(Status::CODE_OK));
+                        $this->writeJson(Status::CODE_OK, $vc->vodId."cdata-----", Status::getReasonPhrase(Status::CODE_OK));
                             if($vl->vodId==$vc->vodId){
-                                return $this->writeJson(Status::CODE_OK, $vc, Status::getReasonPhrase(Status::CODE_OK));
                                 $data["list"][$kl]->isClick=1;   
                             }
                     }
