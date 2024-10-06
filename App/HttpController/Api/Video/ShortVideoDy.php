1d0645abb6165518a069c73623bfe03d2af4c9b0
jinike <ji4926129@gmail.com>
2024-10-06 22:14:33 +0800

    短视频

--- App/HttpController/Api/Video/ShortVideoDy.php
+++ App/HttpController/Api/Video/ShortVideoDy.php
@@ -153,7 +153,7 @@ class ShortVideoDy extends UserBase
                 // }
                 //是否已点击-心过
                 $this->writeJson(Status::CODE_OK, $vodIdArray, Status::getReasonPhrase(Status::CODE_OK));
-                $clickRes=ShortVideoDyClickRecordModel::create()->where(["uid"=>$userId])->where(["vod_id",$vodIdArray,"in"])->get()->lastQuery();
+                $clickRes=ShortVideoDyClickRecordModel::create()->where(["uid"=>$userId])->where(["vod_id",$vodIdArray,"in"])->get();
                 return $this->writeJson(Status::CODE_OK, $clickRes, Status::getReasonPhrase(Status::CODE_OK));
                 foreach($data["list"] as $kl=>$vl){
                     foreach($clickRes as $kc=>$vc){
