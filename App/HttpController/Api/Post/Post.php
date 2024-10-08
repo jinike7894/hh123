43571bf4c74b91c49a71ebad7a81f0986db4c7d1
jinike <ji4926129@gmail.com>
2024-10-08 12:29:00 +0800

    帖子

--- App/HttpController/Api/Post/Post.php
+++ App/HttpController/Api/Post/Post.php
@@ -290,11 +290,11 @@ class Post extends UserBase
                     break;
                 case 2:
                     //点赞回复
-                    $postReplyData=PostModel::create()->where(["id"=>$param["postId"]])->lockForUpdate()->get();
+                    $postReplyData=PostReplyModel::create()->where(["id"=>$param["postId"]])->lockForUpdate()->get();
                     if($postReplyData->uid!==$userId){
                         throw new Exception('取消失败', Status::CODE_INTERNAL_SERVER_ERROR);
                     }
-                    PostModel::create()->where(["id"=>$param["postId"]])->update(["reply"=>$postReplyData["reply"]-1]);
+                    PostReplyModel::create()->where(["id"=>$param["postId"]])->update(["click"=>$postReplyData["click"]-1]);
                     break;
                 default:
                 throw new Exception('错误的type', Status::CODE_BAD_REQUEST);
