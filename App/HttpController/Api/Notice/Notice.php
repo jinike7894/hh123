fdab97124810a1854d01a7099d9c7b1c03540d1b
jinike <ji4926129@gmail.com>
2024-10-09 14:10:48 +0800

    用户中心

--- App/HttpController/Api/Notice/Notice.php
+++ App/HttpController/Api/Notice/Notice.php
@@ -36,7 +36,7 @@ class Notice extends UserBase
             $model = NoticeModel::create();
             $queryBuild = new QueryBuilder();
             $sql="select id,type,title,content,create_at from notice where (property=1 or uid=".$userId.") and is_del  = 0  order by create_at desc LIMIT ".$page.", ".$pageSize;
-            $queryBuild->raw($sql, [1]);
+            $queryBuild->raw($sql);
             $data = DbManager::getInstance()->query($queryBuild);
         } catch (Throwable $e) {
             return $this->writeJson($e->getCode(), [], $e->getMessage());
