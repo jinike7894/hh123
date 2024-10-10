1f9620c551cfa4f103b88fb98176b269ccc5e382
jinike <ji4926129@gmail.com>
2024-10-11 02:05:25 +0800

    关注

--- App/HttpController/Api/Post/Post.php
+++ App/HttpController/Api/Post/Post.php
@@ -178,12 +178,12 @@ class Post extends UserBase
                 $postIdArray[]=$v->post_id;
                 $result["replyData"]["list"][$k]->isClick=0;
             }
-            $this->writeJson(Status::CODE_OK, $postIdArray, Status::getReasonPhrase(Status::CODE_OK));
+
             $ClickRes=PostClickRecordModel::create()->where(["uid"=>$userId,"type"=>2])->where("post_id",$postIdArray,"in")->all();
-            return $this->writeJson(Status::CODE_OK, $ClickRes, Status::getReasonPhrase(Status::CODE_OK));
+           
             foreach($result["list"] as $kl=>$vl){
                 foreach($ClickRes as $kc=>$vc){
-                        if($vl->id==$vc["post_id"]){
+                        if($vl->post_id==$vc["post_id"]){
                             $result["replyData"]["list"][$kl]->isClick=1;   
                         }
                 }
