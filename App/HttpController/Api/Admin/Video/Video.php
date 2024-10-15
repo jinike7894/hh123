25866bd5e3c82e6661b3dcf9ab8db273ee85ce2b
jinike <ji4926129@gmail.com>
2024-10-15 16:02:41 +0800

    后台视频

--- App/HttpController/Api/Admin/Video/Video.php
+++ App/HttpController/Api/Admin/Video/Video.php
@@ -39,11 +39,12 @@ class Video extends AdminBase
                 ->where(["vod_status"=>1])
                 ->order("vod_id","desc")
                 ->getAll($page, $keyword, $pageSize, $field);
-            // if($data["list"]){
-            //     // foreach($data["list"] as $k=>$v){
-            //     //     // $data["list"][$k]=$this->convertKeysToCamelCase($v);
-            //     // }
-            // }
+            if($data["list"]){
+                foreach($data["list"] as $k=>$v){
+                    $data["list"][$k]->vod_time_add=date("Y-m-d H:i:s",$v->vod_time_add);
+                    // $data["list"][$k]=$this->convertKeysToCamelCase($v);
+                }
+            }
         } catch (Throwable $e) {
             return $this->writeJson($e->getCode(), [], $e->getMessage());
         }
