2e2e593452f488377012eaa405f42759eb0d1d1d
jinike <ji4926129@gmail.com>
2024-10-16 23:15:15 +0800

    tag时间

--- App/HttpController/Api/Admin/Upload.php
+++ App/HttpController/Api/Admin/Upload.php
@@ -55,7 +55,6 @@ class Upload extends AdminBase
     try {
         if(!$this->isValidUrl($param['url'])){
             $config=ConfigModel::create()->where("cfgKey",["AwsS3Host","AwsS3Bucket"],"in")->all();
-            return $this->writeJson(Status::CODE_OK, $config, '地址');
             $urlString="";
             foreach($config as $k=>$v){
                 $urlString.=$v["cfgValue"];
