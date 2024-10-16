67e824de9bcaf1e77157079444b1d4a036717714
jinike <ji4926129@gmail.com>
2024-10-16 21:58:40 +0800

    tag时间

--- App/Service/Oss/AwsOssService.php
+++ App/Service/Oss/AwsOssService.php
@@ -120,14 +120,14 @@ class AwsOssService
         // 如果要防止图片被抓的话，这里可以改为其他后缀。
         //$fileName = Func::CreateGuid() . '.' . $mediaType;
         $fileName = Func::CreateGuid() . '.' . 'jpg';
-        $this->s3Client->putObject([
+        $res=$this->s3Client->putObject([
             'Bucket' => $this->s3Config[OssConfigKey::AWS_S3_BUCKET],
             'Key' => $path . DIRECTORY_SEPARATOR . $fileName,
             //'Key' => $fileName,
             'Body' => base64_encode($uploadFile->getStream()), // 原生使用这个 fopen('/path/to/image.jpg', 'r'),
             'ContentType' => $uploadFile->getClientMediaType(), // 必须要加这个才能以图片返回。（否则是下载文件）
         ]);
-
+        throw new Exception($res, Status::CODE_BAD_REQUEST);
         return [
             'path' => DIRECTORY_SEPARATOR . $path . DIRECTORY_SEPARATOR . $fileName,
             //'path' => DIRECTORY_SEPARATOR . $fileName,
