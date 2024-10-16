8082bd83bf5bf662588978e8caeaa8cf1530dfb4
jinike <ji4926129@gmail.com>
2024-10-16 15:37:43 +0800

    账号密码

--- App/HttpController/Api/Admin/CustomerService/CustomerService.php
+++ App/HttpController/Api/Admin/CustomerService/CustomerService.php
@@ -29,15 +29,15 @@ class CustomerService extends AdminBase
                 ->order("create_at","desc")
                 ->all();
             foreach($result["list"] as $k=>$v){
-                    switch($v->use_type){
+                    switch($v["use_type"]){
                         case 1:
-                            $result["list"][$k]->use_type="广告合作";
+                            $result["list"][$k]["use_type"]="广告合作";
                             break;
                        case 2:
-                            $result["list"][$k]->use_type="商务合作";
+                            $result["list"][$k]["use_type"]="商务合作";
                             break;
                         case 3:
-                            $result["list"][$k]->use_type="客服";
+                            $result["list"][$k]["use_type"]="客服";
                             break;
                     }
             }
