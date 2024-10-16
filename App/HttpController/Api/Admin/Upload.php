78f13df935167b311b4be2d95b9c97fac2371bb8
jinike <ji4926129@gmail.com>
2024-10-16 23:23:00 +0800

    tag时间

--- App/HttpController/Api/Admin/Upload.php
+++ App/HttpController/Api/Admin/Upload.php
@@ -67,7 +67,7 @@ class Upload extends AdminBase
             }
             $param['url']=$AwsS3Host.$AwsS3Bucket.$param['url'];
         }
-
+        $this->writeJson(Status::CODE_OK, $param['url'], '上传成功');
         $fileData=file_get_contents($param['url']);
        
         // $result = AwsOssService::getInstance()->uploadFile($this->request(), $param['type']);
