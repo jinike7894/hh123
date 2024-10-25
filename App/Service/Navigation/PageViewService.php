5b1569b8144e4cb8a9ba6c53f87a0c7963c6c774
jinike <ji4926129@gmail.com>
2024-10-25 15:22:03 +0800

    轮播

--- App/Service/Navigation/PageViewService.php
+++ App/Service/Navigation/PageViewService.php
@@ -99,6 +99,7 @@ class PageViewService
                 try {
                     DbManager::getInstance()->startTransactionWithCount();
                     $pageStatic=PageStatisticModel::create()->where(["pageId"=>$pageId,'date' => $date])->lockForUpdate()->get();
+                    file_put_contents('test.json', json_encode($pageStatic)."\r\n",FILE_APPEND);
                     if($pageStatic){
                         //更新
                         $updatePage=[
